a7731252f0dbfac463909a9ec8692e185118430b
Gthalles <garbelottithalles@gmail.com>
2022-03-14 19:33:36 -0300

    Add icons to activated routes

--- src/components/Dashboard/Dashboard.spec.tsx
+++ src/components/Dashboard/Dashboard.spec.tsx
@@ -4,21 +4,39 @@ import { Dashboard } from ".";
 
 describe("Dashboard Component", () => {
 	test("Images must be rendered", () => {
-		render(<Dashboard />);
+		render(<Dashboard  />);
 
 		const imagesAlt = screen.getByAltText("CBLOL-Events logo");
 
 		const eventsIcon = screen.getAllByAltText("Ícone de Eventos");
-
-		const myEventsIcon = screen.getAllByAltText("Ícone de meus Eventos");
-
+		
 		const accountIcon = screen.getAllByAltText("Ícone de Perfil");
 
 		const logoutIcon = screen.getAllByAltText("Ícone de Logout");
 
-        
+		const statisticIcon = screen.getAllByAltText("Ícone de Estatísticas");
 
-		expect([imagesAlt, eventsIcon, myEventsIcon, accountIcon, logoutIcon]).toBeInTheDocument;
+		const newEventIcon = screen.getAllByAltText("Ícone de Novo Evento");
+
+		const recepcionistIcon =  screen.getAllByAltText("Ícone de Recepcionistas");
+
+		const newRecepcionistIcon = screen.getAllByAltText("Ícone de Novo Recepcionista");
+
+		// const userDashboard = screen.getByTitle("user-dashboard");
+
+		// const recepcionistDashboard = screen.getByTitle("recepcionist-dashboard");
+
+		expect([ imagesAlt, eventsIcon, accountIcon, logoutIcon, statisticIcon, newEventIcon, recepcionistIcon, newRecepcionistIcon ]).toBeInTheDocument;
 	});
 
+	test("Title and classes must be in the document", () => {
+		render(<Dashboard />);
+		// const userDashboard = screen.getByTitle("user-dashboard");
+
+		// const recepcionistDashboard = screen.getByTitle("recepcionist-dashboard");
+
+		const adminDashboard = screen.getByTitle("admin-dashboard");
+
+		expect(adminDashboard).toBeInTheDocument;
+	});
 });
